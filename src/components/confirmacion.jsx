a55a9725bf52cbb3c0c583238393a01f5604b6ed
UrpirioDev <133790157+Urpirio@users.noreply.github.com>
2024-12-23 17:32:27 -0400

    Aqui manejan y se despliegan las ventadas de confirmacion de registro y la del formulario de registro.

---
+++ src/components/confirmacion.jsx
@@ -0,0 +1,37 @@
+
+import '../style/forget.css';
+import React, {useState} from 'react';
+import FormOrigen from './prevent.jsx';
+import FormOrigenEnd from './prevent2.jsx';
+
+    function Confirmacion2(){
+         
+        let [M_confir, setM_confir] = useState(true)
+
+        function Sconfir(){
+       
+            setM_confir(!M_confir)
+
+        }
+
+        return(
+                <div class= "Login-boby" >
+                    <div className='form'  >
+                      
+                       {M_confir ? <FormOrigen/> : <FormOrigenEnd/> }
+
+                     <div className='formulario' >
+                    
+                     <button id='btn-r' onClick={Sconfir}>{M_confir ? "Registrar" : "Volver"}</button>
+                    
+                     </div>
+                    
+                    </div>
+                </div>
+        )
+        
+    }
+    
+    export default Confirmacion2
+
+
