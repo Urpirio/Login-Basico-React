944d416a2c3260b15dd7f544986ed8c0df3b902e
UrpirioDev <133790157+Urpirio@users.noreply.github.com>
2024-12-23 17:33:37 -0400

    Formulario login, parte 1

---
+++ src/components/Login.jsx
@@ -0,0 +1,27 @@
+import Forget from './forget';
+
+function Login(){
+    return(
+        <div>
+            <div className='formulario'>
+    <input id='usuario' type="text" placeholder='usuario' />
+</div>
+
+  <div className='formulario' >
+    
+    <input id='Email' type="Email" placeholder='Email' />
+
+  </div>
+
+  <div className='formulario' >
+
+    <input id='password' type="password" placeholder='Contraseña' />
+
+  </div>
+  <div>
+    <Forget/>
+  </div>
+        </div>
+    )
+}
+export default Login
