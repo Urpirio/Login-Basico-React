6a5a2e277fd71a5839b4ddc49385aeaf56eb1060
UrpirioDev <133790157+Urpirio@users.noreply.github.com>
2024-12-23 17:34:14 -0400

    Este componente contiene la imagen de confirmacion.

---
+++ src/components/mensaje.jsx
@@ -0,0 +1,10 @@
+import aceptar  from '../aceptar.png'
+import '../style/forget.css'
+
+function  MensajeD(){
+    return <div className='MensajeD' > 
+
+<img src={aceptar} alt="Imagen de confirmacion" />
+    </div>
+}
+export default MensajeD
