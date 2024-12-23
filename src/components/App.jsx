c01e35b9484bd761b5ad6406380e1f543d739cf3
UrpirioDev <133790157+Urpirio@users.noreply.github.com>
2024-12-23 17:31:07 -0400

    Componente que va conectado al index, el cual renderiza la pagina.

---
+++ src/components/App.jsx
@@ -0,0 +1,17 @@
+
+import Confirmacion2 from './confirmacion.jsx'
+import Tlogin from './TLogin.jsx';
+
+function App() {
+
+  return (
+    <div>
+
+    <Confirmacion2 />
+    
+    <Tlogin/>
+  </div>
+  )
+}
+
+export default App
