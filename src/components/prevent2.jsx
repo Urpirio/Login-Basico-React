3627b105fde96e5b99d06726ff7819118aa071f5
UrpirioDev <133790157+Urpirio@users.noreply.github.com>
2024-12-23 17:35:34 -0400

    Componente que contiene y el gracias, tambien la imagen de confirmacion.

---
+++ src/components/prevent2.jsx
@@ -0,0 +1,14 @@
+import MensajeD from './mensaje.jsx';
+
+function FormOrigenEnd(){
+    return(
+        <div class= "Login-boby2" >
+            <h2>Gracias por registrarte</h2>   
+            <div className='Confirmacion-img'>
+    <MensajeD />
+  </div>
+
+        </div>
+    )
+}
+export default FormOrigenEnd
